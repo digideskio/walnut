4f0dc6dde618e4f5c0c80ce1161aa7a83fdca3cf
AJ ONeal <awesome@coolaj86.com>
2016-03-28 19:50:36 -0400

    add upstart

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo mkdir -p /srv/walnut/{certs,core,lib}
+sudo mkdir -p /srv/walnut/packages/{api,pages,services}
+sudo chown -R $(whoami):$(whoami) /srv/walnut
+
+#git clone git@github.com:Daplie/walnut.git
+git clone https://github.com/Daplie/walnut.git /srv/walnut/core
+
+pushd /srv/walnut/core
+npm install
+rsync -av /srv/walnut/core/etc/init/walnut.conf /etc/init/walnut.conf
+
+popd
+mv /srv/walnut/core/node_modules /srv/walnut
+
+sudo service walnut stop
+sudo service walnut start
